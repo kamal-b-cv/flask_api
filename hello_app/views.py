d67fdc2902b0c2c1a0e583e46da3790ae9810d18
kamal-b-cv <159109257+kamal-b-cv@users.noreply.github.com>
2024-03-09 17:36:45 +0530

    Update views.py

--- hello_app/views.py
+++ hello_app/views.py
@@ -3,16 +3,8 @@ from flask import Flask, render_template
 from . import app
 
 @app.route("/")
-def home():
-    return render_template("home.html")
-
-@app.route("/about/")
-def about():
-    return render_template("about.html")
-
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
+def index():
+    return render_template("index.html")
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
